f3df767eb09670363801b8ec2172a1a26f72cddd
localadmin <localadmin@frodo.f4tech.com>
2013-11-21 10:17:50 -0700

    yet more debugging

--- test/cookbooks/vncserver_test/recipes/default.rb
+++ test/cookbooks/vncserver_test/recipes/default.rb
@@ -1,11 +1,29 @@
-['buildslave-1', 'analyticsslave-1', 'fooser-5'].each do |username|
+# ['buildslave-1', 'analyticsslave-1', 'fooser-5'].each do |username|
 
-  user username
-  vncserver_xstartup username
-  vncserver_passwd username do
-    passwd 'foo'
-  end
+#   # user username
+#   # vncserver_xstartup username
+#   # vncserver_passwd username do
+#   #   passwd 'foo'
+#   # end
 
+# end
+
+user 'buildslave-1'
+vncserver_xstartup 'buildslave-1'
+vncserver_passwd 'buildslave-1' do
+  passwd 'foo'
+end
+
+user 'analyticsslave-1'
+vncserver_xstartup 'analyticsslave-1'
+vncserver_passwd 'analyticsslave-1' do
+  passwd 'foo'
+end
+
+user 'fooser-5'
+vncserver_xstartup 'fooser-5'
+vncserver_passwd 'fooser-5' do
+  passwd 'foo'
 end
 
 vncserver_servers 'vncservers' do
